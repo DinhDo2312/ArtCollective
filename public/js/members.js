087fe1f8e94cd60d221018f079ec38d63c8ade79
Hoff <haffedmcnair@gmail.com>
2018-09-14 11:41:24 -0700

    good for someone liek me

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,38 @@ $(document).ready(function() {
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
+
+  // $("#sexySlick").slick({
+  //   slide: '.slider-item',
+  //   arrows: true,
+  //   appendArrows: '.slider-container .slider-nav',
+  //   prevArrow: '<span class:"slick-prev"></span>',
+  //   nextArrow: '<span class="slick-next"></span>'
+
+
+  // })
+
+
+    $('#sexySlick').each(function() {
+      var _this = $(this);
+    $(this).slick({
+      slide: '.slider-item',
+      arrows: true,
+      prevArrow: 'prev-slide',
+      nextArrow: 'next-slide',
+      appendArrows: ($(_this).hasClass('no-append')) ? '' : $('.slider-nav', _this),
+    });
+    });
+
+    $('.prev-slide').on('click', function (e) {
+      e.preventDefault();
+      $('.#sexySlick').slick('slickPrev');
+    });
+  
+    $('.next-slide').on('click', function (e) {
+      e.preventDefault();
+      $('#sexySlick').slick('slickNext');
+    });
+
 });
+
